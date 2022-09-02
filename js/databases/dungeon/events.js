e2691c2b8455531b5df0907b33a2a172e9cc93ca
Francesco Cottone <kesiev@gmail.com>
2022-09-02 13:38:49 +0200

    :abcd: Fixed typo

--- js/databases/dungeon/events.js
+++ js/databases/dungeon/events.js
@@ -280,7 +280,7 @@ function loadDungeonEvents(MOD) {
             ]
         },{
             id:"eventGambler",
-            codex:"The Gambler: select a card, draw a card and compare mana values to win or lose a card from hand.",
+            codex:"The Gambler: select a card, draw a card, and compare mana values to win or lose a card from hand.",
             steps:[
                 [
                     {
